59e8e537594f1e79619f136ef9e60b5f2db3bbaa
anakrusis <howtofamitracker@gmail.com>
2020-07-11 02:59:27 -0400

    project setup

---
+++ src/main/java/org/adggfjggfafafafa/cells/Camera.java
@@ -0,0 +1,8 @@
+package org.adggfjggfafafafa.cells;
+
+public class Camera {
+    double x; double y; double zoom;
+    public Camera ( double x, double y, double zoom ) {
+        this.x = x; this.y = y; this.zoom = zoom;
+    }
+}
---
+++ src/main/java/org/adggfjggfafafafa/cells/CellsTest.java
@@ -0,0 +1,60 @@
+package org.adggfjggfafafafa.cells;
+
+import processing.core.PApplet;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class CellsTest extends PApplet {
+
+    HashMap<UUID, Cell> cells = new HashMap<>();
+    Camera camera = new Camera( 0 , 0 , 1);
+
+    public void settings() {
+        size(420,420);
+    }
+
+    public void setup() {
+
+        Cell cell = new Cell(0, 0);
+        cell.setUuid( UUID.randomUUID() );
+        cells.put(cell.getUuid(), cell);
+    }
+
+    public void draw() {
+
+        background(204);
+
+        if (keyPressed){
+            if (key == 'w') {
+                camera.y--;
+            } else if (key == 's') {
+                camera.y++;
+            } else if (key == 'a') {
+                camera.x--;
+            } else if (key == 'd') {
+                camera.x++;
+            } else if (key == 'q') {
+                camera.zoom += 0.1;
+            } else if (key == 'e') {
+                camera.zoom -= 0.1;
+            }
+        }
+        camera.zoom = Math.max( camera.zoom, 0.1 );
+
+        translate(width/2f, height/2f);
+        scale( (float) camera.zoom );
+        translate( (float)-camera.x, (float)-camera.y );
+
+        ellipseMode( CENTER );
+        for (Cell cell : cells.values()){
+            ellipse( (float)cell.getX(), (float)cell.getY(), 5, 5 );
+        }
+    }
+
+    public static void main(String... args){
+        String[] processingArgs = {"CellsTest"};
+        CellsTest applet = new CellsTest();
+        PApplet.runSketch(processingArgs, applet);
+    }
+}
